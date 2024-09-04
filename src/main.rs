35786f16badee1da7089443b6ef03fc887a1c496
medfahmy <medfahmy99@gmail.com>
2024-09-04 16:31:09 +0100

    rendering

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,145 @@
 use leptos::*;
+use std::marker::PhantomData;
 
 fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
-    mount_to_body(|| view! { <App /> })
+    // mount_to_body(|| view! { 
+    //     <SizeOf<u8> />
+    //     <br />
+    //     <SizeOf<usize> />
+    //     <br />
+    //     <SizeOf<String> />
+    // })
+
+    mount_to_body(|| view! {
+        <ManyCounters />
+    })
 }
 
 #[component]
-fn App() -> impl IntoView {
+fn ManyCounters() -> impl IntoView {
+    let len = 5;
+    let counters = (1..=len).map(|n| create_signal(n));
+
+    let counter_buttons = counters
+        .map(|(count, set_count)| {
+            view! {
+                <li>
+                    <button on:click=move |_| set_count(count() + 1)>{count}</button>
+                </li>
+            }
+        })
+        .collect_view();
+
+    view! {
+        <ul>{counter_buttons}</ul>
+    }
+}
+
+#[component]
+fn Collect() -> impl IntoView {
+    let words = vec!["Hello", "Beautiful", "World"];
+
+    mount_to_body(|| view! { 
+        <p>{words.clone()}</p>
+        <ul>
+            {words
+                .into_iter()
+                .map(|word| view! { <li>{word}</li> })
+                // .collect::<Vec<_>>()}
+                .collect_view()}
+        </ul>
+    })
+}
+
+#[component]
+fn AppInto() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+    let double_count = move || count() * 2;
+
+    view! {
+        <button on:click=move |_| { set_count.update(|n| *n += 1) }>
+            "Increment"
+        </button>
+
+        <button on:click=move |_| { set_count.update(|n| if *n > 0 { *n -= 1 }) }>
+            "Decrement"
+        </button>
+
+        <p 
+            // class:red=move || count() % 2 == 1
+            class=("red", move || count() % 2 == 1)
+        >
+            {count}
+        </p>
+
+        <p>
+            {double_count}
+        </p>
+
+        <SimpleProgress progress=count.into() />
+        <SimpleProgress progress=Signal::derive(double_count) />
+    }
+}
+
+#[component]
+fn SimpleProgress(progress: Signal<i32>) -> impl IntoView {
+    view! {
+        <progress max="50" value=progress />
+    }
+}
+
+
+#[component]
+fn SizeOf<T: Sized>(#[prop(optional)] _ty: PhantomData<T>) -> impl IntoView {
+    std::mem::size_of::<T>()
+}
+
+#[component]
+fn Progress(
+    #[prop(optional)] optional: u16,
+    #[prop(default = 100)] max: u16,
+    #[prop(into)] progress: Signal<i32>,
+) -> impl IntoView {
+    view! {
+        <progress max=max value=progress />
+    }
+}
+
+#[component]
+fn App2() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+    let double_count = move || count() * 2;
+
+    view! {
+        <button on:click=move |_| { set_count.update(|n| *n += 1) } >
+            "Increment"
+        </button>
+
+        <button on:click=move |_| { set_count.update(|n| if *n > 0 { *n -= 1 }) } >
+            "Decrement"
+        </button>
+
+        <p 
+            // class:red=move || count() % 2 == 1
+            class=("red", move || count() % 2 == 1)
+        >
+            {count}
+        </p>
+
+        <p>
+            {double_count}
+        </p>
+
+        <Progress progress=count />
+        <Progress progress=Signal::derive(double_count) />
+    }
+}
+
+#[component]
+fn App0() -> impl IntoView {
     let (count, set_count) = create_signal(0);
     let double_count = move || count() * 2;
 
@@ -63,5 +194,13 @@ fn App1() -> impl IntoView {
             >
             "Click to Move"
             </button>
+    }
+}
+
+#[component]
+fn Raw() -> impl IntoView {
+    let html = "<p>This HTML will be injected.</p>";
+    view! {
+        <div inner_html=html/>
     }
 }
